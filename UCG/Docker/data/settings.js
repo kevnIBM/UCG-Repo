3b4c171c9039941c9680c5524e68066e1aa1cc5a
Dennis  Noto <dennisnoto@gmail.com>
2017-09-10 23:42:11 -0600

    updated key files

--- UCG/Docker/data/settings.js
+++ UCG/Docker/data/settings.js
@@ -18,7 +18,6 @@
 // to make it available:
 //var fs = require("fs");
 
-
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
     uiPort: process.env.PORT || 1880,
