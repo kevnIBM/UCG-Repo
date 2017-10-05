1bea7bf0491ce0e862dad223e926bc33be7c08d8
Dennis  Noto <dennisnoto@gmail.com>
2017-10-05 14:54:14 -0600

    updated build

--- UCG/Docker/UCGBuild.sh
+++ UCG/Docker/UCGBuild.sh
@@ -5,11 +5,19 @@
     # copy setting.js, flows.json, flows_cred.json from Docker/data/ to node-red-docker/data
 
 # Build command for creating the container for volume mapping via the run command
-docker build -f latest/Dockerfile-Volume-Mapped -t dennisnotojr/node-red-docker-node8-nr-17:version-3 .
+docker build -f latest/Dockerfile-Volume-Mapped -t dennisnotojr/node-red-docker-node8-nr-17-data-mapped:version-1 .
+
+# Build command for creating the container for volume mapping via the run command and using cluster service
+docker build -f latest/Dockerfile-Volume-Mapped-Cluster -t dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster:version-1 .
 
 # Build command for creating the container with the data directory copied into the container
-docker build -f latest/Dockerfile-Data-In-Container -t dennisnotojr/nodered-loadtest:version17 .
+docker build -f latest/Dockerfile-Data-In-Container -t dennisnotojr/node-red-docker-node8-nr-17-data-container:version-1 .
+
+# Build command for creating the conatiner with the data directory copied into the container and using cluster service
+docker build -f latest/Dockerfile-Data-In-Container-Cluster -t dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-1 .
 
 #Push containers to docker hub
-docker push dennisnotojr/node-red-docker-node8-nr-17:version-3
-docker push dennisnotojr/nodered-loadtest:version17
+docker push dennisnotojr/node-red-docker-node8-nr-17-data-mapped:version-1
+docker push dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster:version-1
+docker push dennisnotojr/node-red-docker-node8-nr-17-data-container:version-1
+docker push dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-1
