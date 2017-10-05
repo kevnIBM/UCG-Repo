a96c86f1d1a69715f2f96d46b75a2977fbb034d0
Dennis  Noto <dennisnoto@gmail.com>
2017-10-05 15:39:36 -0600

    upated files again

--- UCG/Docker/UCGRun.sh
+++ UCG/Docker/UCGRun.sh
@@ -1,10 +1,15 @@
 
 #Run command for container with data volume mapped
-docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/UCG-Repo/UCG/Docker/data:/data  -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17:version-3
+docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/UCG-Repo/UCG/Docker/data:/data  -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-mapped:version-1
+
+#Run command for container with data volume mapped and cluster
+docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/UCG-Repo/UCG/Docker/data:/data  -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster:version-1
 
 #Run command for container with data in the container
-docker run -d -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/nodered-loadtest:version17
+docker run -d -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-container:version-1
 
+#Run command for container with data in the container and cluster
+docker run -d -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-1
 
 #Run command for container with data in the container and logging to syslog
  docker run -d -p 1880:1880  \
@@ -12,4 +17,4 @@ docker run -d -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_spa
     --log-opt tag="{{ (.ExtraAttributes nil).appName }}" \
     --log-opt env=appName \
     -e appName=NodeRed -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 \
-    dennisnotojr/nodered-loadtest:version17
+    dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster:version-1
