3500eb8f5c00b47195cf74fae9a2e5f4f40deba7
Dennis  Noto <dennisnoto@gmail.com>
2017-11-01 10:06:49 -0600

    opps forgot to change the run file

--- UCG/Docker/UCGRun.sh
+++ UCG/Docker/UCGRun.sh
@@ -1,15 +1,15 @@
 
 #Run command for container with data volume mapped
-docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/UCG-Repo/UCG/Docker/data:/data  -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-mapped:version-1
+docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/UCG-Repo/UCG/Docker/data:/data  -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-mapped:version-3
 
 #Run command for container with data volume mapped and cluster
-docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/UCG-Repo/UCG/Docker/data:/data  -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster:version-1
+docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/UCG-Repo/UCG/Docker/data:/data  -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster:version-3
 
 #Run command for container with data in the container
-docker run -d -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-container:version-1
+docker run -d -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-container:version-3
 
 #Run command for container with data in the container and cluster
-docker run -d -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-1
+docker run -d -p 1880:1880  -e NODE_ENV=production -e NODE_OPTIONS=--max_old_space_size=1024 dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-3
 
 #Run command for container with data in the container and logging to syslog
  docker run -d -p 1880:1880  \
