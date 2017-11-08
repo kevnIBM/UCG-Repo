cb306e3327d39f2c0a8cc18ee03dc9cab26a008a
Dennis  Noto <dennisnoto@gmail.com>
2017-11-08 09:27:23 -0700

    changed to alpine for all containers

--- UCG/Docker/UCGBuild.sh
+++ UCG/Docker/UCGBuild.sh
@@ -3,24 +3,19 @@
 # copy setting.js, flows.json, flows_cred.json from Docker/data/ to node-red-docker/data for data-container
 
 # Build command for creating the container for volume mapping via the run command
-docker build -f latest/Dockerfile-Data-Volume-Mapped -t dennisnotojr/node-red-docker-node8-nr-17-data-mapped:version-3 .
+docker build -f latest/Dockerfile-Data-Volume-Mapped -t dennisnotojr/node-red-docker-node8-nr-17-data-mapped:version-4 .
 
 # Build command for creating the container for volume mapping via the run command and using cluster service
-docker build -f latest/Dockerfile-Data-Volume-Mapped-Cluster -t dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster:version-3 .
+docker build -f latest/Dockerfile-Data-Volume-Mapped-Cluster -t dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster:version-4 .
 
 # Build command for creating the container with the data directory copied into the container
-docker build -f latest/Dockerfile-Data-In-Container -t dennisnotojr/node-red-docker-node8-nr-17-data-container:version-3 .
+docker build -f latest/Dockerfile-Data-In-Container -t dennisnotojr/node-red-docker-node8-nr-17-data-container:version-4 .
 
 # Build command for creating the conatiner with the data directory copied into the container and using cluster service
-docker build -f latest/Dockerfile-Data-In-Container-Cluster -t dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-3 .
+docker build -f latest/Dockerfile-Data-In-Container-Cluster -t dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-4 .
 
 #Push containers to docker hub
-docker push dennisnotojr/node-red-docker-node8-nr-17-data-mapped:version-3
-docker push dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster:version-3
-docker push dennisnotojr/node-red-docker-node8-nr-17-data-container:version-3
-docker push dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-3
-
-
-docker build -f latest/dockerfile-Node-Ubuntu -t dennisnotojr/node-ubuntu:version-1 .
-
-docker build -f latest/Dockerfile-Data-Volume-Mapped-Cluster-Alpine -t dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster-alpine:version-1 .
+docker push dennisnotojr/node-red-docker-node8-nr-17-data-mapped:version-4
+docker push dennisnotojr/node-red-docker-node8-nr-17-data-mapped-cluster:version-4
+docker push dennisnotojr/node-red-docker-node8-nr-17-data-container:version-4
+docker push dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-4
