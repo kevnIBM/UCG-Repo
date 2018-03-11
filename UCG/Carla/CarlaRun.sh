767bbef906c8b85ab0e86f26b5c7b12112daa353
Dennis  Noto <dennisnoto@gmail.com>
2018-03-10 21:31:47 -0700

    cleanup

--- UCG/Carla/CarlaRun.sh
+++ UCG/Carla/CarlaRun.sh
@@ -1,10 +1,10 @@
 
 #Run command for container with data volume mapped
-docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/Carla/carla-docker/htdocs:/opt/htdocs  -p 80:80  -p 443:443 -e WEB_DOCUMENT_ROOT=/opt/htdocs/  dennisnotojr/carla-apache-2-php-7-data-mapped:version-1
+docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/UCG-Repo/UCG/Carla/carla-docker/htdocs:/opt/htdocs  -p 80:80  -p 443:443 -e WEB_DOCUMENT_ROOT=/opt/htdocs/  dennisnotojr/carla-apache-2-php-7-data-mapped:version-1
 
 
 #Run with logging to syslog
-docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/Carla/carla-docker/htdocs:/opt/htdocs  -p 80:80  -p 443:443  \
+docker run -d -v /Users/dennisnoto/Documents/NotoData-Dev/UCG-Repo/UCG/Carla/carla-docker/htdocs:/opt/htdocs  -p 80:80  -p 443:443  \
     --log-driver syslog \
     --log-opt tag="{{ (.ExtraAttributes nil).appName }}" \
     --log-opt env=appName \
