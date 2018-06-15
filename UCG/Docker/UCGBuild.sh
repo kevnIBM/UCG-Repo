6a8a41bcbe7b80196d56af5eb024556092666be3
Dennis  Noto <dennisnoto@gmail.com>
2018-06-15 08:31:53 -0600

    update repo with some new npms that Im testing

--- UCG/Docker/UCGBuild.sh
+++ UCG/Docker/UCGBuild.sh
@@ -3,19 +3,19 @@
 # copy setting.js, flows.json, flows_cred.json from Docker/data/ to node-red-docker/data for data-container
 
 # Build command for creating the container for volume mapping via the run command
-docker build -f latest/Dockerfile-Data-Volume-Mapped -t dennisnotojr/node-red-docker-node8-nr-18-data-mapped:version-4 .
+docker build -f latest/Dockerfile-Data-Volume-Mapped -t dennisnotojr/node-red-docker-node8-nr-18-data-mapped:version-6 .
 
 # Build command for creating the container for volume mapping via the run command and using cluster service
-docker build -f latest/Dockerfile-Data-Volume-Mapped-Cluster -t dennisnotojr/node-red-docker-node8-nr-18-data-mapped-cluster:version-4 .
+docker build -f latest/Dockerfile-Data-Volume-Mapped-Cluster -t dennisnotojr/node-red-docker-node8-nr-18-data-mapped-cluster:version-5 .
 
 # Build command for creating the container with the data directory copied into the container
-docker build -f latest/Dockerfile-Data-In-Container -t dennisnotojr/node-red-docker-node8-nr-17-data-container:version-4 .
+docker build -f latest/Dockerfile-Data-In-Container -t dennisnotojr/node-red-docker-node8-nr-18-data-container:version-5 .
 
 # Build command for creating the conatiner with the data directory copied into the container and using cluster service
-docker build -f latest/Dockerfile-Data-In-Container-Cluster -t dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-4 .
+docker build -f latest/Dockerfile-Data-In-Container-Cluster -t dennisnotojr/node-red-docker-node8-nr-18-data-container-cluster:version-5 .
 
 #Push containers to docker hub
-docker push dennisnotojr/node-red-docker-node8-nr-18-data-mapped:version-4
-docker push dennisnotojr/node-red-docker-node8-nr-18-data-mapped-cluster:version-4
-docker push dennisnotojr/node-red-docker-node8-nr-17-data-container:version-4
-docker push dennisnotojr/node-red-docker-node8-nr-17-data-container-cluster:version-4
+docker push dennisnotojr/node-red-docker-node8-nr-18-data-mapped:version-6
+docker push dennisnotojr/node-red-docker-node8-nr-18-data-mapped-cluster:version-5
+docker push dennisnotojr/node-red-docker-node8-nr-18-data-container:version-5
+docker push dennisnotojr/node-red-docker-node8-nr-18-data-container-cluster:version-5
