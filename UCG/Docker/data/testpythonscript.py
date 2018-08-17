e543b26ea53f6a2a287a71de3f243a6786f5dd0b
Dennis  Noto <dennisnoto@gmail.com>
2018-08-16 18:53:55 -0600

    Working on spacy python example

--- UCG/Docker/data/testpythonscript.py
+++ UCG/Docker/data/testpythonscript.py
@@ -1,16 +1,19 @@
 import sys
 import spacy
 from spacy.attrs import LOWER, POS, ENT_TYPE, IS_ALPHA
-print ("python works")
+
+text = u'IBM built the first artifical intelligence system called WATSON.'
+print(text)
 
 nlp = spacy.load('en_core_web_sm')
-doc = nlp(u'IBM built the first artifical intelligence system called WATSON.')
+doc = nlp(text)
 
-#for token in doc:
-#    print(token.text, token.dep_, token.head.text, token.head.pos_,
-#          [child for child in token.children])
+for token in doc:
+    print(token.text, token.dep_, token.head.text, token.head.pos_,
+          [child for child in token.children])
 
-#for ent in doc.ents:
-#    print(ent.text, ent.start_char, ent.end_char, ent.label_)
-np_array = doc.to_array([LOWER, POS, ENT_TYPE, IS_ALPHA])
-print(np_array)
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+
+#np_array = doc.to_array([LOWER, POS, ENT_TYPE, IS_ALPHA])
+#print(np_array)
